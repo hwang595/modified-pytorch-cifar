57731aa14123aba5d4b8747df0fd04bf606e608f
hwang595 <hongyiwang@cs.wisc.edu>
2022-07-19 22:44:12 -0400

    debugging fused operation2

--- models/resnet.py
+++ models/resnet.py
@@ -112,8 +112,12 @@ class FusedLowRankBasicBlock(nn.Module):
         rank1 = min(in_planes, planes)
         rank2 = min(planes, planes)
 
-        self.conv1 = FusedConvKernel(in_planes, planes, padding=1, stride=stride, factorization_ratio=0.25)
-        self.conv2 = FusedConvKernel(planes, planes, padding=1, stride=1)
+        self.conv1 = FusedConvKernel(in_planes, planes, kernel_size=3, 
+                        factorization_ratio=0.25, padding=1, stride=stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = FusedConvKernel(planes, planes, kernel_size=3, 
+                                    factorization_ratio=0.25, padding=1, stride=1)
+        self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -124,8 +128,8 @@ class FusedLowRankBasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = self.conv2(out)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
